69dc07a7a7776e8f7ba7243a8bf0739d0e45818a
lotusshinoaki <tatsuya@lotusandcompanyinc.com>
2022-09-19 07:04:13 +0900

    Improve variable names

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use clap::Parser;
 struct Args {
     #[clap(short, long, value_parser, default_value_t = u32::MAX)]
     tries: u32,
-    #[clap(short, long, value_parser, default_value_t = 0)]
-    delay: u64,
-    #[clap(short, long, value_parser, default_value_t = u64::MAX)]
-    max_delay: u64,
+    #[clap(short, long = "delay", value_parser, default_value_t = 0)]
+    delay_s: u64,
+    #[clap(short, long = "max-delay", value_parser, default_value_t = u64::MAX)]
+    max_delay_s: u64,
     #[clap(short, long, value_parser, default_value_t = 1)]
     backoff: u64,
     #[clap(short, long, value_parser, default_value_t = false)]
@@ -24,7 +24,7 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let mut next_delay = args.delay;
+    let mut next_delay_s = args.delay_s;
     let mut count = 0u32;
     loop {
         let mut command = Command::new(&args.utility);
@@ -55,11 +55,11 @@ fn main() {
         count += 1;
 
         if !args.quiet {
-            println!("Retrying({}): Wait {} seconds", count, next_delay);
+            println!("Retrying({}): Wait {} seconds", count, next_delay_s);
         }
-        thread::sleep(time::Duration::from_secs(next_delay));
+        thread::sleep(time::Duration::from_secs(next_delay_s));
 
-        next_delay *= args.backoff;
-        next_delay = cmp::min(next_delay, args.max_delay);
+        next_delay_s *= args.backoff;
+        next_delay_s = cmp::min(next_delay_s, args.max_delay_s);
     }
 }
